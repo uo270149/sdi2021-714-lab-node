5d4cd9d5c4cdb467331363f6ef513817a1f4f0f4
uo270149 <sofiasuarezfdez@gmail.com>
2021-03-30 12:43:41 +0200

    SDI-2021-714-8.1-Acceso a datos de Mongo desde Node.js.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 let express = require('express');
 let app = express();
 
+let mongo = require("mongodb");
 let swig = require('swig');
 let bodyParser = require('body-parser');
 app.use(bodyParser.json());
@@ -11,10 +12,11 @@ app.use(express.static('public'));
 
 // Variables
 app.set('port', 8081); // puerto en el que se despliega la aplicación
+app.set('db', 'mongodb://admin:sdi@tiendamusica-shard-00-00.bz4oc.mongodb.net:27017,tiendamusica-shard-00-01.bz4oc.mongodb.net:27017,tiendamusica-shard-00-02.bz4oc.mongodb.net:27017/tiendamusica?ssl=true&replicaSet=atlas-ohpo1y-shard-0&authSource=admin&retryWrites=true&w=majority');
 
 // Rutas/controladores por lógica
 require("./routes/rusuarios.js")(app, swig) // (app, param1, param2, etc.)
-require("./routes/rcanciones.js")(app, swig) // (app, param1, param2, etc.)
+require("./routes/rcanciones.js")(app, swig, mongo) // (app, param1, param2, etc.)
 require("./routes/rautores.js")(app, swig) // (app, param1, param2, etc.)
 
 // Lanzar el servidor
--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -1,4 +1,4 @@
-module.exports = function (app, swig) {
+module.exports = function (app, swig, mongo) {
     app.get('/canciones/agregar', function (req, res) {
         let respuesta = swig.renderFile('views/bagregar.html', {});
         res.send(respuesta);
@@ -42,9 +42,27 @@ module.exports = function (app, swig) {
     });
 
     app.post("/cancion", function (req, res) {
-        res.send("Canción agregada: " + req.body.nombre + "<br>"
-            + " género: " + req.body.genero + "<br>"
-            + " precio: " + req.body.precio);
+        let cancion = {
+            nombre: req.body.nombre,
+            genero: req.body.genero,
+            precio: req.body.precio
+        }
+        // Conectarse
+        mongo.MongoClient.connect(app.get('db'), function (err, db) {
+            if (err) {
+                res.send("Error de coneción: " + err);
+            } else {
+                let collection = db.collection('canciones');
+                collection.insertOne(cancion, function (err, result) {
+                    if (err) {
+                        res.send("Error al insertar " + err);
+                    } else {
+                        res.send("Agregada id: " + result.ops[0]._id);
+                    }
+                    db.close();
+                });
+            }
+        });
     });
 
     app.get('/promo*', function (req, res) {
